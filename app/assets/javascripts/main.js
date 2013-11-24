27d958afde6660896a26580bfadad567264c30b5
Ilya Rubnich <irubnich@gmail.com>
2013-11-24 12:07:41 -0500

    support for multiple currencies

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,23 +1,47 @@
 $(function() {
-		function getPrice() {
-			var price = $("#price");
-			price.html("I'M FUCKING UPDATING HOLD YOUR SHIT...");
-			$.ajax({
-				url: "/USD",
-				success: function(data) {
-					// Get date
-					var date = moment();
 
-					// Parse price
-					var priceData = parseFloat(data).toFixed(2);
+	// Magic price function
+	getPrice = function(currencyCode) {
+		var price = $("#price span#the-price");
+		price.html("HOLD YOUR SHIT I'M UPDATING");
+		$.ajax({
+			url: "/" + currencyCode,
+			success: function(data) {
+				// Get date
+				var date = moment();
 
-					// Set HTML
-					price.html("GODDAMN SON IT'S FUCKING<br>$" + priceData);
-					$("#last-updated span").html(date.format("hh:mm:ss A"));
-					$("title").html("$" + priceData + " | WHAT'S THE FUCKING PRICE OF BITCOIN?");
-				}
-			});
-		}
-		getPrice();
-		setInterval(getPrice, 20000);
+				// Parse price
+				var priceData = parseFloat(data).toFixed(2).replace(/./g, function(c, i, a) {
+    				return i && c !== "." && !((a.length - i) % 3) ? "," + c : c;
+				});
+
+				// Set HTML
+				price.html(priceData);
+				$("#last-updated span").html(date.format("hh:mm:ss A"));
+				$("title").html(priceData + " " + currencyCode + " | WHAT'S THE FUCKING PRICE OF BITCOIN?");
+			},
+			error: function() {
+
+			}
+		});
+	}
+
+	// Select box
+	$('#currency').customSelect();
+
+	// Events
+	$("#currency").change(function() {
+		getPrice(this.value);
 	});
+
+	// Initial go
+	var selectedCurrency = $("#currency")[0].value;
+	getPrice(selectedCurrency);
+
+	// Interval
+	setInterval(function() {
+		var selectedCurrency = $("#currency")[0].value;
+		getPrice(selectedCurrency);
+	}, 20000);
+
+});
--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,4 +1,7 @@
 class SiteController < ApplicationController
+
+	CURRENCIES = ["AFN", "ALL", "DZD", "AOA", "ARS", "AMD", "AWG", "AUD", "AZN", "BSD", "BHD", "BDT", "BBD", "BYR", "BZD", "BMD", "BTN", "BOB", "BAM", "BWP", "BRL", "GBP", "BND", "BGN", "BIF", "KHR", "CAD", "CVE", "KYD", "XAF", "XPF", "CLP", "CNY", "COP", "KMF", "CDF", "CRC", "HRK", "CUP", "CZK", "DKK", "DJF", "DOP", "XCD", "EGP", "ERN", "EEK", "ETB", "EUR", "FKP", "FJD", "GMD", "GEL", "GHS", "GHS", "GIP", "GTQ", "GNF", "GYD", "HTG", "HNL", "HKD", "HUF", "ISK", "INR", "IDR", "IRR", "IQD", "ILS", "JMD", "JPY", "JPY", "JOD", "KZT", "KES", "KWD", "KGS", "LAK", "LVL", "LBP", "LSL", "LRD", "LYD", "LTL", "MOP", "MKD", "MGA", "MWK", "MYR", "MVR", "MRO", "MUR", "MXN", "MDL", "MNT", "MAD", "MZN", "MMK", "NAD", "NPR", "ANG", "TWD", "NZD", "NIO", "NGN", "KPW", "NOK", "OMR", "PKR", "PAB", "PGK", "PYG", "PEN", "PHP", "PLN", "QAR", "RON", "RUB", "RWF", "SHP", "SVC", "WST", "SAR", "RSD", "SCR", "SLL", "SGD", "SBD", "SOS", "ZAR", "KRW", "LKR", "SDG", "SRD", "SZL", "SEK", "CHF", "SYP", "STD", "TJS", "TZS", "THB", "TOP", "TTD", "TND", "TRY", "TMM", "UGX", "UAH", "AED", "USD", "UYU", "UZS", "VUV", "VEF", "VND", "XOF", "YER", "ZMK", "ZWL"]
+
 	def price
 		# set currency
 		unless params.has_key?(:currency)
@@ -25,5 +28,6 @@ class SiteController < ApplicationController
 		render :json => json["btc_to_" + currency_lower]
 	end
 	def index
+		@currencies = CURRENCIES
 	end
 end
