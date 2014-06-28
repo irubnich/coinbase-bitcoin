5c6244d9057b2ad45040dddfa1173d036750491a
Ilya Rubnich <irubnich@gmail.com>
2014-06-27 22:46:18 -0400

    More cleanup

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -13,42 +13,44 @@ var HMIAFB = {
 	},
 
 	sendRequest: function() {
-		var price = $("#price span#the-price");
-		var oldValue = parseFloat(price.html());
-
 		$.ajax({
 			url: "/" + this.currencyCode,
-			success: function(data) {
-				// Check for some problem...
-				if(data.error) {
-					$("#price").html("SHIT, COINBASE FUCKED UP...");
-					setInterval(function() {
-						location.reload();
-					}, 10000);
-					return;
-				}
-
-				// Get date
-				var date = moment();
-
-				// Parse new value
-				var newValue = parseFloat(data);
-
-				// Parse price
-				var priceData = newValue.toFixed(2).replace(/./g, function(c, i, a) {
-    				return i && c !== "." && !((a.length - i) % 3) ? "," + c : c;
-				});
-
-				// Set HTML
-				$("span#expletive").html(this.expletives[Math.floor(Math.random()*this.expletives.length)]);
-				price.html(priceData);
-				$("#last-updated span").html(date.format("hh:mm:ss A"));
-				$("title").html(priceData + " " + this.currencyCode + " | WHAT'S THE FUCKING PRICE OF BITCOIN?");
-			}.bind(this),
+			success: this.updateSite.bind(this),
 			error: function() {
 				$("#price").html("SOMETHING WENT FUCKING WRONG. REFRESH THE PAGE. DO IT!");
 			}
 		});
+	},
+
+	updateSite: function(data) {
+		var price = $("#price span#the-price");
+		var oldValue = parseFloat(price.html());
+
+		// Check for some problem...
+		if(data.error) {
+			$("#price").html("SHIT, COINBASE FUCKED UP...");
+			setInterval(function() {
+				location.reload();
+			}, 10000);
+			return;
+		}
+
+		// Get date
+		var date = moment();
+
+		// Parse new value
+		var newValue = parseFloat(data);
+
+		// Parse price
+		var priceData = newValue.toFixed(2).replace(/./g, function(c, i, a) {
+			return i && c !== "." && !((a.length - i) % 3) ? "," + c : c;
+		});
+
+		// Set HTML
+		$("span#expletive").html(this.expletives[Math.floor(Math.random()*this.expletives.length)]);
+		price.html(priceData);
+		$("#last-updated span").html(date.format("hh:mm:ss A"));
+		$("title").html(priceData + " " + this.currencyCode + " | WHAT'S THE FUCKING PRICE OF BITCOIN?");
 	}
 };
 
